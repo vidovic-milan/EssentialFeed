05a181ec91a7244a57caac82f2ce6f0953ef5fd9
Vidovic, Milan, HSE DE <milan.vidovic@hse.com>
2023-02-12 19:31:55 +0100

    Move code to production

---
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+public final class RemoteFeedLoader {
+    private let client: HTTPClient
+    private let url: URL
+
+    public init(client: HTTPClient, url: URL) {
+        self.client = client
+        self.url = url
+    }
+
+    public func load() {
+        client.get(from: url)
+    }
+}
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
--- EssentialFeedTests/RemoteFeedLeaderTests.swift
+++ EssentialFeedTests/RemoteFeedLeaderTests.swift
@@ -1,23 +1,5 @@
-import Foundation
+import EssentialFeed
 import XCTest
-
-class RemoteFeedLoader {
-    let client: HTTPClient
-    let url: URL
-
-    init(client: HTTPClient, url: URL) {
-        self.client = client
-        self.url = url
-    }
-
-    func load() {
-        client.get(from: url)
-    }
-}
-
-protocol HTTPClient {
-    func get(from url: URL)
-}
 
 class RemoteFeedLoaderTests: XCTestCase {
     func test_init_shouldNotGetFromURL() {
