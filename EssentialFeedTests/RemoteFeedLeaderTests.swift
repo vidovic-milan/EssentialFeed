de939d61c3550cb0b7e9fb94d1b7093cc44926f7
Vidovic, Milan, HSE DE <milan.vidovic@hse.com>
2023-02-12 19:09:36 +0100

    Test requesting URL with singleton

--- EssentialFeedTests/RemoteFeedLeaderTests.swift
+++ EssentialFeedTests/RemoteFeedLeaderTests.swift
@@ -2,18 +2,30 @@ import Foundation
 import XCTest
 
 class RemoteFeedLoader {
-    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")!
+    }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
     var requestedURL: URL?
 }
 
-class RemoteFeedLoaderTests: XCTest {
+class RemoteFeedLoaderTests: XCTestCase {
     func test_init_shouldNotGetFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteFeedLoader()
 
         XCTAssertNil(client.requestedURL)
+    }
+
+    func test_load_shouldRequestFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+
+        sut.load()
+
+        XCTAssertNotNil(client.requestedURL)
     }
 }
