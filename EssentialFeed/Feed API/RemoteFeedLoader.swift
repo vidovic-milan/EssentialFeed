71ce4f77c64e6f4fe82bf9348a5ed4170a2e49a7
Vidovic, Milan, HSE DE <milan.vidovic@hse.com>
2023-02-15 12:07:25 +0100

    Move `RemoteFeedMapper` to a separate file

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -34,34 +34,3 @@ public final class RemoteFeedLoader {
         }
     }
 }
-
-private struct RemoteFeedMapper {
-    private struct Root: Decodable {
-        let items: [Item]
-    }
-
-    private struct Item: Decodable {
-        let id: UUID
-        let description: String?
-        let location: String?
-        let image: URL
-
-        var feedItem: FeedItem { FeedItem(id: id, description: description, location: location, imageURL: image) }
-
-        init(id: UUID, description: String?, location: String?, image: URL) {
-            self.id = id
-            self.description = description
-            self.location = location
-            self.image = image
-        }
-    }
-
-    static var OK_200: Int { 200 }
-
-    static func map(response: HTTPURLResponse, data: Data) throws -> [FeedItem] {
-        guard let root = try? JSONDecoder().decode(Root.self, from: data), response.statusCode == OK_200 else {
-            throw RemoteFeedLoader.Error.invalidData
-        }
-        return root.items.map { $0.feedItem }
-    }
-}
