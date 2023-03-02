0f04ac26e951c9192576d4ae24e5be57e1d986fd
Vidovic, Milan, HSE DE <milan.vidovic@hse.com>
2023-03-02 19:55:15 +0100

    Request from url using URLSession

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -7,14 +7,27 @@ class URLSessionHTTPClient {
     init(client: URLSession) {
         self.client = client
     }
+
+    func get(from url: URL) {
+        client.dataTask(with: url) { _, _, _ in }
+    }
 }
 
 class URLSessionHTTPClientTests: XCTestCase {
-    func test_shouldNotCreateRequestOnInit() {
+    func test_init_shouldNotCreateRequest() {
         let client = URLSessionSpy()
         let _ = URLSessionHTTPClient(client: client)
 
         XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+
+    func test_getFromUrl_shouldRequestFromClient() {
+        let client = URLSessionSpy()
+        let sut = URLSessionHTTPClient(client: client)
+
+        sut.get(from: URL(string: "https://a-url.com")!)
+
+        XCTAssertEqual(client.requestedURLs, [URL(string: "https://a-url.com")!])
     }
 
     private class URLSessionSpy: URLSession {
