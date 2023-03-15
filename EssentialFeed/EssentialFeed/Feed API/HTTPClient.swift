685fc99a22186445f346961f157177bdfb282cb9
Vidovic, Milan, HSE DE <milan.vidovic@hse.com>
2023-03-15 17:50:02 +0100

    Move type alias inside `HTTPClient`

--- EssentialFeed/EssentialFeed/Feed API/HTTPClient.swift
+++ EssentialFeed/EssentialFeed/Feed API/HTTPClient.swift
@@ -1,9 +1,8 @@
 import Foundation
 
-public typealias HTTPClientResult = Result<(Data, HTTPURLResponse), Error>
-
 public protocol HTTPClient {
+    typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
     /// Completion can be invoked on any queue
     /// Clients are responsible to dispatch to appropriate thread, if needed.
-	func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+	func get(from url: URL, completion: @escaping (Result) -> Void)
 }
