5af205ecda6a3e65b44311902090c8e9245b2c5b
Vidovic, Milan, HSE DE <milan.vidovic@hse.com>
2023-03-16 19:53:30 +0100

    Populate cells with prototype data

--- Prototype/Prototype/FeedViewController.swift
+++ Prototype/Prototype/FeedViewController.swift
@@ -8,12 +8,22 @@ struct FeedViewModel {
 
 class FeedViewController: UITableViewController {
 
+    let feed = FeedViewModel.prototype
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 6
+        return feed.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCell(withIdentifier: "Feed Cell")!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Feed Cell") as! FeedTableViewCell
+        cell.descriptionLabel.text = feed[indexPath.row].description
+        cell.descriptionLabel.isHidden = feed[indexPath.row].description == nil
+
+        cell.locationLabel.text = feed[indexPath.row].location
+        cell.locationContainer.isHidden = feed[indexPath.row].location == nil
+
+        cell.feedImageView.image = UIImage(named: feed[indexPath.row].imageName)
+        return cell
     }
 }
 
--- Prototype/Prototype/FeedViewModel+Prototype.swift
+++ Prototype/Prototype/FeedViewModel+Prototype.swift
@@ -3,12 +3,12 @@ import Foundation
 extension FeedViewModel {
     static var prototype: [FeedViewModel] {
         [
-            .init(description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.", location: "Garth Pier\north Wales", imageName: "image-1"),
+            .init(description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.", location: "Garth Pier North Wales", imageName: "image-1"),
             .init(description: nil, location: "Garth Pier\north Wales", imageName: "image-1"),
             .init(description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.", location: nil, imageName: "image-1"),
-            .init(description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.", location: "Garth Pier\north Wales", imageName: "image-1"),
+            .init(description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.", location: "Garth Pier\nNorth Wales", imageName: "image-1"),
             .init(description: nil, location: nil, imageName: "image-1"),
-            .init(description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.", location: "Garth Pier\north Wales", imageName: "image-1")
+            .init(description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.", location: "Garth Pier\nNorth Wales", imageName: "image-1")
         ]
     }
 }
