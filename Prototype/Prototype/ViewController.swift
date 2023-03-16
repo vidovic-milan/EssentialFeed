cf121ac4c435588493074ddc5f213f23650acdab
Vidovic, Milan, HSE DE <milan.vidovic@hse.com>
2023-03-16 19:41:04 +0100

    Render out 6 cells

--- Prototype/Prototype/ViewController.swift
+++ Prototype/Prototype/ViewController.swift
@@ -1,9 +1,13 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "Feed Cell")!
     }
 }
 
