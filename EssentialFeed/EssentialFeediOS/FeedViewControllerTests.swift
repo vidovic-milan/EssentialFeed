f4b7478c042ddc6cad2029fc6f73fa63efd80872
Vidovic, Milan, HSE DE <milan.vidovic@hse.com>
2023-03-17 13:10:32 +0100

    `FeedViewController` does not call load upon creation

--- EssentialFeed/EssentialFeediOS/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOS/FeedViewControllerTests.swift
@@ -1,5 +1,19 @@
 import XCTest
 
+class FeedViewController {
+    init(loader: FeedViewControllerTests.FeedLoaderSpy) {
+    }
+}
+
 class FeedViewControllerTests: XCTestCase {
-    
+    func test_init_doesNotCallLoad() {
+        let loader = FeedLoaderSpy()
+        _ = FeedViewController(loader: loader)
+
+        XCTAssertEqual(loader.loadCallCount, 0)
+    }
+
+    class FeedLoaderSpy {
+        private(set) var loadCallCount = 0
+    }
 }
