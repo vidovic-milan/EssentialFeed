afba3d87d78a6e4b0a8535eedfa96615bb69e51f
Vidovic, Milan, HSE DE <milan.vidovic@hse.com>
2023-03-18 17:21:55 +0100

    Extract adapter code to a separate adapter function

--- EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -7,9 +7,13 @@ public final class FeedUIComposer {
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
         let refreshController = FeedRefreshViewController(feedLoader: feedLoader)
         let controller = FeedViewController(feedRefreshController: refreshController)
-        refreshController.onRefresh = { [weak controller] feed in
+        refreshController.onRefresh = adaptFeedToCellControllers(controller: controller, imageLoader: imageLoader)
+        return controller
+    }
+
+    private static func adaptFeedToCellControllers(controller: FeedViewController, imageLoader: FeedImageDataLoader) -> (([FeedImage]) -> Void) {
+        return { [weak controller] feed in
             controller?.cellControllers = feed.map { FeedImageCellController(model: $0, imageLoader: imageLoader) }
         }
-        return controller
     }
 }
