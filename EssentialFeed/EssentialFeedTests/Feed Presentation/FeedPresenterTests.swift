35a3b129ad85d51012946212d2580b33c15f139a
Vidovic, Milan, HSE DE <milan.vidovic@hse.com>
2023-03-22 19:20:44 +0100

    Did finish loading feed displays no loading and feed

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -5,16 +5,31 @@ struct FeedViewModel {
     let feed: [FeedImage]
 }
 
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
+
 class FeedPresenter {
     private let feedView: FeedView
+    private let loadingView: FeedLoadingView
 
-    init(feedView: FeedView) {
+    init(feedView: FeedView, loadingView: FeedLoadingView) {
         self.feedView = feedView
+        self.loadingView = loadingView
+    }
+
+    func didFinishLoadingFeed(with feed: [FeedImage]) {
+        feedView.display(FeedViewModel(feed: feed))
+        loadingView.display(FeedLoadingViewModel(isLoading: false))
     }
 }
 
 protocol FeedView {
     func display(_ viewModel: FeedViewModel)
+}
+
+protocol FeedLoadingView {
+    func display(_ viewModel: FeedLoadingViewModel)
 }
 
 final class FeedPresenterTests: XCTest {
@@ -25,13 +40,35 @@ final class FeedPresenterTests: XCTest {
         XCTAssertEqual(spy.messages.count, 0)
     }
 
+    func test_didFinishLoadingFeed_displaysFeedAndNoLoading() {
+        let spy = FeedViewSpy()
+        let sut = FeedPresenter(feedView: spy)
+
+        let feed = anyFeed()
+        sut.didFinishLoadingFeed(with: feed)
+
+        XCTAssertEqual(spy.messages, [.feed(feed), .isLoading(false)])
+    }
+
     // MARK: - Helpers
 
-    class FeedViewSpy: FeedView {
-        var messages: [Any] = []
+    private class FeedViewSpy: FeedView, FeedLoadingView {
+        enum Message: Equatable {
+            case isLoading(Bool)
+            case feed([FeedImage])
+        }
+        var messages: [Message] = []
 
         func display(_ viewModel: FeedViewModel) {
-            
+            messages.append(.feed(viewModel.feed))
         }
+
+        func display(_ viewModel: FeedLoadingViewModel) {
+            messages.append(.isLoading(viewModel.isLoading))
+        }
+    }
+
+    private func anyFeed() -> [FeedImage] {
+        return [FeedImage(id: UUID(), description: nil, location: nil, url: URL(string: "https://a-url.com")!)]
     }
 }
