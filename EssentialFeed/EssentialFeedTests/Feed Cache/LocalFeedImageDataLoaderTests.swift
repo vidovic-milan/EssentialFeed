474819ca51f77c9dd6abf603c30767d6f89c4256
Vidovic, Milan, HSE DE <milan.vidovic@hse.com>
2023-07-02 09:22:09 +0200

    `LocalFeedImageDataLoader.loadImage` requests stored data for url from store

--- EssentialFeed/EssentialFeedTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
@@ -1,13 +1,19 @@
 import XCTest
 import EssentialFeed
 
-protocol FeedImageDataStore {}
+protocol FeedImageDataStore {
+    func retrieve(dataFor url: URL)
+}
 
 class LocalFeedImageDataLoader {
     private let store: FeedImageDataStore
 
     init(store: FeedImageDataStore) {
         self.store = store
+    }
+
+    func loadImage(from url: URL) {
+        store.retrieve(dataFor: url)
     }
 }
 
@@ -20,10 +26,26 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [])
     }
 
+    func test_loadImageFromURL_requestsStoredDataForURL() {
+        let store = FeedImageStoreSpy()
+        let sut = LocalFeedImageDataLoader(store: store)
+        let url = anyURL()
+
+        sut.loadImage(from: url)
+
+        XCTAssertEqual(store.receivedMessages, [.retrieve(dataFor: url)])
+    }
+
     // MARK: - Helpers
 
     private class FeedImageStoreSpy: FeedImageDataStore {
-        enum Message: Equatable {}
+        enum Message: Equatable {
+            case retrieve(dataFor: URL)
+        }
         var receivedMessages: [Message] = []
+
+        func retrieve(dataFor url: URL) {
+            receivedMessages.append(.retrieve(dataFor: url))
+        }
     }
 }
