1e660dbb4f2daa2d4489bb6c7b383bf3a745b97c
Vidovic, Milan, HSE DE <milan.vidovic@hse.com>
2023-07-02 12:01:23 +0200

    Added new core data model version to support persisting data

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/Core Data/ManagedFeedImage.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/Core Data/ManagedFeedImage.swift
@@ -6,6 +6,7 @@ class ManagedFeedImage: NSManagedObject {
     @NSManaged var id: UUID
     @NSManaged var location: String?
     @NSManaged var imageDescription: String?
+    @NSManaged var data: Data?
     @NSManaged var url: URL
     @NSManaged var cache: ManagedCache
 }
