06119ac2954bd19f8e73d14e0972cf201e7df80b
Vidovic, Milan, HSE DE <milan.vidovic@hse.com>
2023-09-22 17:07:32 +0200

    Displays empty feed on launch when customer has no connectivity and no cache

--- EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
+++ EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
@@ -27,7 +27,9 @@ class FeedAcceptanceTests: XCTestCase {
     }
 
     func test_onLaunch_displaysEmptyFeedWhenCustomerHasNoConnectivityAndNoCache() {
+        let feed = launch(httpClient: .offline, store: .empty)
 
+        XCTAssertEqual(feed.numberOfVisibleFeedViews, 0)
     }
 
     // MARK: - Helpers
