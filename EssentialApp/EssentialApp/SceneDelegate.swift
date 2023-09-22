03168494ff6636987faff1a63383d3163325a701
Vidovic, Milan, HSE DE <milan.vidovic@hse.com>
2023-09-22 18:37:41 +0200

    Remove unnecessary Main storyboard - the initial set up happens in code

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -27,8 +27,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
 
+        window = UIWindow(windowScene: scene)
         configureWindow()
     }
 
@@ -57,5 +58,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                     fallback: FeedImageDataLoaderCacheDecorator(
                         decoratee: remoteImageLoader,
                         cache: localImageLoader))))
+
+        window?.makeKeyAndVisible()
     }
 }
--- EssentialApp/EssentialAppTests/SceneDelegateTests.swift
+++ EssentialApp/EssentialAppTests/SceneDelegateTests.swift
@@ -18,4 +18,21 @@ class SceneDelegateTests: XCTestCase {
         XCTAssertTrue(topController is FeedViewController, "Expected a feed controller as top view controller, got \(String(describing: topController)) instead")
     }
 
+    func test_configureWindow_setsWindowAsKeyAndVisible() {
+        let window = UIWindowSpy()
+        let sut = SceneDelegate()
+        sut.window = window
+
+        sut.configureWindow()
+
+        XCTAssertEqual(window.makeKeyAndVisibleCallCount, 1, "Expected to make window key and visible")
+    }
+
+    private class UIWindowSpy: UIWindow {
+      var makeKeyAndVisibleCallCount = 0
+      override func makeKeyAndVisible() {
+        makeKeyAndVisibleCallCount = 1
+      }
+    }
+
 }
